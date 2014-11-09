61d95f5c192e14bb051f05a15d0d8f4ad689f1fd
Molly White <mwhite@hubspot.com>
2014-11-08 21:02:46 -0500

    Write more!

--- bot.py
+++ bot.py
@@ -25,44 +25,60 @@ import tweepy
 from secrets import *
 from random import choice
 
-queries = ['"nobody likes me"',
-           '"nobody loves me"']
+data = {
+    'like': {
+        'queries': ['"nobody likes me"',
+                    '"nobody loves me"'],
+        'responses': ['I like you.',
+                      'You\'re valued.',
+                      u'\u2764']
+    }
+}
 
 filters = ['http',
            '#nowplaying',
            'youtube',
            '-',
            '"',
-           '“',
-           '”']
+           u'“',
+           u'”']
 
-def get_tweet():
+def get_tweet(api, type):
     '''Get a list of tweets matching the search query.'''
+    query = choice(data[type]['queries'])
+    results = api.search(q=query, count=50)
+    return results
+
+def get_users():
+    '''Get a list of users we've already tweeted at.'''
+    f = open('tweeted_users.txt', 'r')
+    users = [line.rstrip('\n') for line in f]
+    return users
+
+def filter_tweets(tweets, users):
+    '''Filter out tweets to avoid things like song lyrics, etc.'''
+    page = 1
+    while True:
+        tweet = tweets.pop(0)
+        text = tweet.text
+        if not (any(substr in text for substr in filters) or tweet.author.screen_name in users):
+            return tweet
+        if len(tweets) == 0:
+            return
+
+def send_reply(api, type, tweet):
+    text = '@' + tweet.author.screen_name + ' ' + choice(data[type]['responses'])
+    print(text)
+    #api.update_status(text, in_reply_to_status_id = tweet.id_str)
+
+if __name__ == "__main__":
     auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
     auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
     api = tweepy.API(auth)
 
-    query = choice(queries)
-    results = api.search(q=query)
-    return results
+    type = choice(data.keys())
 
-def filter_tweets(tweets):
-    '''Filter out tweets to avoid things like song lyrics, etc.'''
-    while True:
-        tweet = tweets.pop(0)
-        text = tweet.text
-        if any(substr in text for substr in filters):
-            print("filtered: " + text)
-        else:
-            print(text)
-        if len(tweets) == 0:
-            break
-
-def send_reply(tweet):
-    print(tweet)
-
-
-if __name__ == "__main__":
-    tweets = get_tweet()
-    tweet = filter_tweets(tweets)
-    send_reply(tweet)
+    tweets = get_tweet(api, type)
+    users = get_users()
+    tweet = filter_tweets(tweets, users)
+    send_reply(api, type, tweet)
