435644528763befa72475006a9bb54ffa443c392
Molly White <molly.white5@gmail.com>
2014-11-16 14:59:46 -0500

    Update gitignore, style fixes

--- bot.py
+++ bot.py
@@ -26,20 +26,18 @@ from secrets import *
 from random import choice
 import os
 
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 tweeted_file = os.path.join(__location__, "tweeted_users.txt")
 
-data = {
-    'like': {
-        'queries': ['"nobody likes me"',
-                    '"nobody loves me"'],
-        'responses': ['I like you.',
-                      'You\'re valued.',
-                      'You matter.',
-                      u'\u2764']
-    }
-}
+data = {'like':
+            {'queries': ['"nobody likes me"',
+                         '"nobody loves me"'],
+             'responses': ['I like you.',
+                           'You\'re valued.',
+                           'You matter.',
+                           u'\u2764']
+            }
+        }
 
 filters = ['http',
            '#nowplaying',
@@ -49,45 +47,53 @@ filters = ['http',
            u'“',
            u'”']
 
-def get_tweet(api, type):
-    '''Get a list of tweets matching the search query.'''
-    query = choice(data[type]['queries'])
-    results = api.search(q=query, count=50)
+
+def get_tweet(api_, type_):
+    """Get a list of tweets matching the search query."""
+    query = choice(data[type_]['queries'])
+    results = api_.search(q=query, count=50)
     return results
 
-def get_users():
-    '''Get a list of users we've already tweeted at.'''
-    f = open(tweeted_file, 'r')
-    users = [line.rstrip('\n') for line in f]
-    return users
 
-def filter_tweets(tweets, users):
-    '''Filter out tweets to avoid things like song lyrics, etc.'''
-    page = 1
+def get_users():
+    """Get a list of users we've already tweeted at."""
+    f = open(tweeted_file, 'r')
+    usrs = [line.rstrip('\n') for line in f]
+    return usrs
+
+
+def filter_tweets(tweets_, users_):
+    """Filter out tweets to avoid things like song lyrics, etc."""
     while True:
-        tweet = tweets.pop(0)
-        text = tweet.text
-        if not (hasattr(tweet, "retweeted_status") or tweet.in_reply_to_status_id or tweet.author.screen_name in users or any(substr in text for substr in filters)):
-            return tweet
+        tweet_ = tweets_.pop(0)
+        text = tweet_.text
         if len(tweets) == 0:
             return
+        if not (hasattr(tweet_, "retweeted_status") or
+                tweet_.in_reply_to_status_id or
+                tweet_.author.screen_name in users_ or
+                any(substr in text for substr in filters)):
+            return tweet_
 
-def send_reply(api, type, tweet):
-    '''Send the reply tweet and record it.'''
+
+def send_reply(api_, type_, tweet_):
+    """Send the reply tweet and record it."""
     f = open(tweeted_file, 'a')
-    f.write(tweet.author.screen_name + '\n')
+    f.write(tweet_.author.screen_name + '\n')
     f.close()
-    text = '@' + tweet.author.screen_name + ' ' + choice(data[type]['responses'])
-    api.update_status(text, in_reply_to_status_id = tweet.id_str)
+    text = '@' + tweet_.author.screen_name + ' ' + choice(data[type_]['responses'])
+    api_.update_status(text, in_reply_to_status_id=tweet_.id_str)
+
 
 if __name__ == "__main__":
+    """Find a tweet and reply to it."""
     auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
     auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
     api = tweepy.API(auth)
 
-    type = choice(data.keys())
+    tweet_type = choice(data.keys())
 
-    tweets = get_tweet(api, type)
+    tweets = get_tweet(api, tweet_type)
     users = get_users()
     tweet = filter_tweets(tweets, users)
-    send_reply(api, type, tweet)
+    send_reply(api, tweet_type, tweet)
