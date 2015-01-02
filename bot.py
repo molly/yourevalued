172bf39f32a98054cc29ec89e00bcabe233a13db
Molly White <molly.white5@gmail.com>
2015-01-01 19:24:14 -0500

    Update copyright dates

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2014 Molly White
+# Copyright (c) 2014–2015 Molly White
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
